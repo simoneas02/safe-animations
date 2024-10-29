edab659a4a33a60db25977df286cc549a1f93711
Simone Amorim <simoneas03@gmail.com>
2024-10-29 12:41:06 +0100

    chore: Update the next and ts config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,50 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // `Image` not supported in static export
+  images: {
+    unoptimized: true,
+  },
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: false,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: { removeViewBox: false },
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+        },
+      ],
+    })
+    // New rule for .glb files
+    config.module.rules.push({
+      test: /\.glb$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/media/[hash][ext][query]',
+      },
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
