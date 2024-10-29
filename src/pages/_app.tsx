540df2cc3d4f805a5cc6697b60ad5176b247024b
Simone Amorim <simoneas03@gmail.com>
2024-10-29 12:47:09 +0100

    feat: Config the theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CacheProvider } from '@emotion/react'
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendMuiTheme,
+} from '@mui/material/styles'
+import type { EmotionCache } from '@emotion/react'
+import { createEmotionCache } from '@/styles/emotion'
+import { CssBaseline } from '@mui/material'
+import { theme as baseTheme } from '@/styles/theme'
+import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+const cssVarsTheme = extendMuiTheme(baseTheme)
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: AppProps & {
+  emotionCache?: EmotionCache
+}) {
+  return (
+    <>
+      <CssVarsProvider theme={cssVarsTheme}>
+        <CssBaseline />
+      </CssVarsProvider>
+      <Component {...pageProps} />
+    </>
+  )
 }
